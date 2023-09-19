e18cc12899b227735187c50e5d1609c388bba5ea
Fahim Ihtesham Sohan <fisohan7@gmail.com>
2023-09-20 02:12:58 +0600

    init

---
+++ Models/TodoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace todo.Models
+{
+    public class TodoModel
+    {
+        public required string title { get; set; }
+        public string? description { get; set; }
+        public bool isDone { get; set; } = false;
+        public DateTime createTime { get; set; } = DateTime.Now;
+    }
+}
